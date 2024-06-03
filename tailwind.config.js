460bdfa03f1a1b76316cecd0d66b0b4864e5b268
Sheldon Kotyk <sheldon@kotyk.com>
2024-06-02 19:48:26 -0500

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,59 @@ export default {
     ],
 
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'headline': ['Assistant', 'sans-serif'],
+                'title': ['Assistant', 'sans-serif'],
+                'regular': ['Assistant', 'sans-serif'],
+            },
+            padding: {
+                '29': '7rem',
+                '-29': '-7rem',
+                '-36': '-10rem'
+            },
+            margin: {
+                '29': '7rem',
+                '36': '10rem',
+                '72': '22rem',
+                '128': '48rem',
+                '-22': '-5rem',
+                '-26': '-6rem',
+                '-29': '-7rem',
+                '-36': '-10rem',
+                '-72': '-22rem',
+                '-74': '-24rem',
+                '-82': '-28rem',
+            },
+            height: {
+                '300': '300px',
+                '350': '350px',
+                '500': '500px',
+                '750': '750px',
+            },
+            width: {
+                '500': '500px',
+            },
+            zIndex: {
+                '0': '0',
+                '10': '10',
+                '-10': '-10',
+                '20': '20',
+                '30': '30',
+                '40': '40',
+                '50': '50',
+                '25': '25',
+                '50': '50',
+                '75': '75',
+                '100': '100',
+                'auto': 'auto',
+            },
+        },
     },
 
     plugins: [
         require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 };
