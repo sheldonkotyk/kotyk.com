a52d1a4a2f90fea9a4fa5e90a8566549af120e6e
Sheldon Kotyk <sheldon@kotyk.com>
2024-09-02 18:01:11 -0500

    redis queue

--- config/queue.php
+++ config/queue.php
@@ -104,9 +104,7 @@ return [
     */
 
     'failed' => [
-        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'sqlite'),
-        'table' => 'failed_jobs',
+        'driver' => env('QUEUE_FAILED_DRIVER', 'file'),
+        // 'storage_path' => storage_path('failed-jobs'),
     ],
-
 ];
