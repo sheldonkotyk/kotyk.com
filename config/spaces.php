f51a70f40b6378453cf8c668ee2a689484318ee6
Sheldon Kotyk <sheldon@kotyk.com>
2025-05-30 12:01:43 -0500

    add spaces

---
+++ config/spaces.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'secret_key' => env('SPACES_SECRET_KEY'),
+    'slug' => env('SPACES_SLUG'),
+    'js_endpoint' => env('SPACES_JS_ENDPOINT', 'https://spaces.run/script/external.js'),
+
+];
